2e54e3dcca5dbfaf5987f41cc09c3c9c499451f7
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-11-21 18:39:38 -0500

    Add user id to search request

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,7 +2,7 @@ function doSearch(e) {
   e.preventDefault();
   var formInput = $("#search input[type='text']").val();
   var userId = $('#search').attr('data-user');
-
+  console.log()
   if (formInput.indexOf('@') > -1) {
     var term = formInput.replace('@', '');
     $.ajax({
